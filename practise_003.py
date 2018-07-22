585f9f2a8421abd4a991a23c89f8cb0627e9953c
learmorepython <0101lch@163.com>
2018-07-22 22:29:17 +0800

    Modify practise_003.py

--- practise_003.py
+++ practise_003.py
@@ -14,40 +14,43 @@ def death():
 
 
 def central_corridor():
-    print "The Gothons of Planet Percal #25 have invaded your ship and destroyed"
-    print "your entire crew. You are the last surviving member and your last"
-    print "mission is to get the neutron destruct bomb from the Weapons Armory,"
-    print "put it in the bridge, and blow the ship up after getting into an "
-    print "escape pod."
-    print "\n"
-    print "You're running down the central corridor to the Weapons Armory when"
-    print "a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume"
-    print"flowing around his hate filled body. He's blocking the door to the"
-    print "Armory and about to pull a weapon to blast you."
+    """
+    You're running down the central corridor to the Weapons Armory when
+    a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume
+    flowing around his hate filled body. He's blocking the door to the
+    Armory and about to pull a weapon to blast you.
+    """
+    print central_corridor.__doc__
     action = raw_input("> ")
     if action == "shoot!":
-        print "Quick on the draw you yank out your blaster and fire it at the Gothon."
-        print "His clown costume is flowing and moving around his body, which throws"
-        print "off your aim. Your laser hits his costume but misses him entirely. This"
-        print "completely ruins his brand new costume his mother bought him, which"
-        print "makes him fly into an insane rage and blast you repeatedly in the face until"
-        print "you are dead. Then he eats you."
+        print """
+        Quick on the draw you yank out your blaster and fire it at the Gothon.
+        His clown costume is flowing and moving around his body, which throws
+        off your aim. Your laser hits his costume but misses him entirely. This
+        completely ruins his brand new costume his mother bought him, which
+        makes him fly into an insane rage and blast you repeatedly in the face until
+        you are dead. Then he eats you.
+        """
         return 'death'
     elif action == "dodge!":
-        print "Like a world class boxer you dodge, weave, slip and slide right"
-        print "as the Gothon's blaster cranks a laser past your head."
-        print "In the middle of your artful dodge your foot slips and you"
-        print "bang your head on the metal wall and pass out."
-        print "You wake up shortly after only to die as the Gothon stomps on"
-        print "your head and eats you."
+        print """
+        Like a world class boxer you dodge, weave, slip and slide right
+        as the Gothon's blaster cranks a laser past your head.
+        In the middle of your artful dodge your foot slips and you
+        bang your head on the metal wall and pass out.
+        You wake up shortly after only to die as the Gothon stomps on
+        your head and eats you.
+        """
         return 'death'
     elif action == "tell a joke":
-        print "Lucky for you they made you learn Gothon insults in the academy."
-        print "You tell the one Gothon joke you know:"
-        print "Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr, fur fvgf nebhaq gur ubhfr."
-        print "The Gothon stops, tries not to laugh, then busts out laughing and can't move."
-        print "While he's laughing you run up and shoot him square in the head"
-        print "putting him down, then jump through the Weapon Armory door."
+        print """
+        Lucky for you they made you learn Gothon insults in the academy.
+        You tell the one Gothon joke you know:
+        Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr, fur fvgf nebhaq gur ubhfr.
+        The Gothon stops, tries not to laugh, then busts out laughing and can't move.
+        While he's laughing you run up and shoot him square in the head
+        putting him down, then jump through the Weapon Armory door.
+        """
         return 'laser_weapon_armory'
     else:
         print "DOES NOT COMPUTE!"
@@ -55,30 +58,36 @@ def central_corridor():
 
 
 def the_bridge():
-    print "You burst onto the Bridge with the neutron destruct bomb"
-    print "under your arm and surprise 5 Gothons who are trying to"
-    print"take control of the ship. Each of them has an even uglier"
-    print"clown costume than the last. They haven't pulled their"
-    print "weapons out yet, as they see the active bomb under your"
-    print "arm and don't want to set it off."
+    """
+    You burst onto the Bridge with the neutron destruct bomb
+    under your arm and surprise 5 Gothons who are trying to
+    take control of the ship. Each of them has an even uglier
+    clown costume than the last. They haven't pulled their
+    weapons out yet, as they see the active bomb under your
+    arm and don't want to set it off.
+    """
+    print the_bridge.__doc__
     action = raw_input("> ")
     if action == "throw the bomb":
-        print "In a panic you throw the bomb at the group of Gothons"
-        print"and make a leap for the door. Right as you drop it a"
-        print "Gothon shoots you right in the back killing you."
-        print "As you die you see another Gothon frantically try to disarm"
-        print "the bomb. You die knowing they will probably blow up when"
-        print "it goes off."
+        print """
+        In a panic you throw the bomb at the group of Gothons
+        and make a leap for the door. Right as you drop it a
+        Gothon shoots you right in the back killing you.
+        As you die you see another Gothon frantically try to disarm
+        the bomb. You die knowing they will probably blow up when it goes off.
+        """
         return 'death'
     elif action == "slowly place the bomb":
-        print "You point your blaster at the bomb under your arm "
-        print "and the Gothons put their hands up and start to sweat."
-        print "You inch backward to the door, open it, and then carefully"
-        print "place the bomb on the floor, pointing your blaster at it."
-        print "You then jump back through the door, punch the close button"
-        print "and blast the lock so the Gothons can't get out."
-        print "Now that the bomb is placed you run to the escape pod to"
-        print "get off this tin can."
+        print """
+        You point your blaster at the bomb under your arm 
+        and the Gothons put their hands up and start to sweat.
+        You inch backward to the door, open it, and then carefully
+        place the bomb on the floor, pointing your blaster at it.
+        You then jump back through the door, punch the close button
+        and blast the lock so the Gothons can't get out.
+        Now that the bomb is placed you run to the escape pod to
+        get off this tin can.
+        """
         return 'escape_pod'
     else:
         print "DOES NOT COMPUTE!"
@@ -86,13 +95,16 @@ def the_bridge():
 
 
 def laser_weapon_armory():
-    print "You do a dive roll into the Weapon Armory, crouch and scan the room"
-    print"for more Got hons that might be hiding. It'sdead quiet, too quiet."
-    print "You stand up and run to the far side of the room and find the"
-    print "neutron bomb in its container. There's a keypad lock on the box"
-    print"and you need the code to get the bomb out. If you get the code"
-    print "wrong 10 times then the lock closes forever and you can't"
-    print "get the bomb. The code is 3 digits."
+    """
+    You do a dive roll into the Weapon Armory, crouch and scan the room
+    for more Got hons that might be hiding. It'sdead quiet, too quiet.
+    You stand up and run to the far side of the room and find the
+    neutron bomb in its container. There's a keypad lock on the box
+    and you need the code to get the bomb out. If you get the code
+    wrong 10 times then the lock closes forever and you can't
+    get the bomb. The code is 3 digits.
+    """
+    print laser_weapon_armory.__doc__
     code = "%d%d%d" % (randint(1, 9), randint(1, 9), randint(1, 9))
     print code
     guess = raw_input("[keypad]> ")
@@ -102,42 +114,48 @@ def laser_weapon_armory():
         guesses += 1
         guess = raw_input("[keypad]> ")
     if guess == code:
-        print "The container clicks open and the seal breaks, letting gas out."
-        print "You grab the neutron bomb and run as fast as you can to the"
-        print "bridge where you must place it in the right spot."
+        print """
+        The container clicks open and the seal breaks, letting gas out.
+        You grab the neutron bomb and run as fast as you can to the
+        bridge where you must place it in the right spot.
+        """
         return 'the_bridge'
     else:
-        print "The lock buzzes one last time and then you hear a sickening"
-        print "melting sound as the mechanism is fused together."
-        print "You decide to sit there, and finally the Gothons blow up the"
-        print "ship from their ship and you die."
+        print """
+        The lock buzzes one last time and then you hear a sickening
+        melting sound as the mechanism is fused together.
+        You decide to sit there, and finally the Gothons blow up the
+        ship from their ship and you die.
+        """
         return 'death'
 
 
 def escape_pod():
-    print "You rush through the ship desperately trying to make it to"
-    print"the escape pod before the whole ship explodes. It seems like"
-    print "hardly any Gothons are on the ship, so your run is clear of"
-    print "interference. You get to the chamber with the escape pods, and"
-    print "now need to pick one to take. Some of them could be damaged"
-    print"but you don't have time to look. There's 5pods, which one"
-    print "do you take?"
+    """
+    You rush through the ship desperately trying to make it to
+    the escape pod before the whole ship explodes. It seems like
+    hardly any Gothons are on the ship, so your run is clear of
+    interference. You get to the chamber with the escape pods, and
+    now need to pick one to take. Some of them could be damaged
+    but you don't have time to look. There's 5pods, which one do you take?
+    """
+    print escape_pod.__doc__
     good_pod = randint(1, 5)
     print good_pod
     guess = raw_input("[pod #]> ")
     if int(guess) != good_pod:
-        print "You jump into pod %s and hit the eject button." % guess
-        print "The pod escapes out into the void of space, then"
-        print "implodes as the hull ruptures, crushing your body"
-        print "into jam jelly."
+        print """
+        You jump into pod %s and hit the eject button.
+        The pod escapes out into the void of space, then
+        implodes as the hull ruptures, crushing your body into jam jelly.""" % guess
         return 'death'
     else:
-        print "You jump into pod %s and hit the eject button." % guess
-        print "The pod easily slides out into space heading print "
-        print "the planet below. As it flies to the planet, you look"
-        print "back and see your ship implode then explode like a "
-        print "bright star, taking out the Gothon ship at the same"
-        print "time. You won!"
+        print """
+        You jump into pod %s and hit the eject button.
+        The pod easily slides out into space heading print 
+        the planet below. As it flies to the planet, you look
+        back and see your ship implode then explode like a 
+        bright star, taking out the Gothon ship at the same time. You won!""" % guess
         exit(0)
 
 
@@ -151,6 +169,13 @@ ROOMS = {
 
 
 def runner(map, start):
+    """
+    The Gothons of Planet Percal #25 have invaded your ship and destroyed
+    your entire crew. You are the last surviving member and your last
+    mission is to get the neutron destruct bomb from the Weapons Armory,
+    put it in the bridge, and blow the ship up after getting into an escape pod.
+    """
+    print runner.__doc__
     next_step = start
     while True:
         room = map[next_step]
