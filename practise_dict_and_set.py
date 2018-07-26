ecb079d1ad58f4dfeb273a4dbafd55d53fe5ebe8
learmorepython <0101lch@163.com>
2018-07-26 23:02:53 +0800

    Add practise_dict_and_set.py

---
+++ practise_dict_and_set.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+practise_dict_and_set
+"""
+
+# dict：初始化后可以修改
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+print "d is:", d
+print "d['Bob']:", d["Bob"]
+d["Bob"] = 90
+print "d is:", d
+print "d['Bob']:", d["Bob"]
+print "get Jack:", d.get("Jack", 60)
+d.pop("Bob")
+print "after pop d is:", d
+
+
+# set：无序和无重复元素的集合
+s = set([1, 1, 2, 2, 3, 3])
+print s
+s.add(4)
+s.add(4)
+print "after add twice:", s
+s.remove(4)
+print "after remove:", s
+
+s1 = set([1, 2, 3])
+s2 = set([2, 3, 4])
+print "s1 & s2:", s1 & s2
+print "s1 | s2:", s1 | s2
+
+
